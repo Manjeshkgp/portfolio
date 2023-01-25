2d662b470a119666606952d986b612f54fd61949
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2023-01-25 23:31:23 +0530

    Education Component Created

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <>
-      <div className="flex flex-col gap-6 items-start mt-12 h-[calc(100vh-12rem)] mx-6 border-b lg:mx-40">
+      <div className="flex flex-col gap-6 items-start mt-24 h-[calc(100vh-12rem)] mx-6 border-b lg:mx-40">
         <p className="text-base text-[#3be6be]">Hi my name is</p>
         <p className="text-3xl font-bold lg:text-[4rem] text-[#ccd6f6] lg:my-2">Manjesh Kumar Sharma</p>
         <p className="text-3xl font-bold lg:text-[4rem] text-[#7c839c] lg:my-2">I build Web Applications</p>
--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const Education = () => {
-  return (
-    <div>Education</div>
-  )
+  return (<>
+    <div className="flex flex-col gap-6 lg:gap-2 items-end mt-12 h-[calc(100vh-12rem)] mx-6 border-b lg:mx-40">
+        <p className="text-base text-[#3be6be]">Higher Secondary Education from KSSHS</p>
+        <p className="text-3xl font-bold lg:text-[4rem] text-[#ccd6f6] lg:my-2 text-right">Science Stream, 2022</p>
+        <p className="text-base text-[#7c839c] lg:w-1/2 lg:my-2">
+          Passed with an overall of 80.2% in Grade 12. I was studying Maths, Chemistry, Physics & Modern Computer Applications during that time.
+        </p>
+        <p className="text-base text-[#3be6be]">High School Education from KTVPHS</p>
+        <p className="text-3xl font-bold lg:text-[4rem] text-[#ccd6f6] lg:my-2 text-right">Matriculation, 2020</p>
+        <p className="text-base text-[#7c839c] lg:w-1/2 lg:my-2">
+          Passed with an overall of 79.8% in Grade 10. I studied a mixture of almost 8 to 10 subjects including 2 languages (Hindi & English).
+        </p>
+      </div>
+  </>)
 }
 
-export default Education
+export default Education;
