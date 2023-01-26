10f6263170c5835370796504d935af10b1ac2de9
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2023-01-26 18:47:16 +0530

    Projects Component Layout Created

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,117 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineGithub, AiOutlineEye } from "react-icons/ai";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <>
+      <div className="flex justify-start items-center flex-wrap mx-6 lg:mx-40 mt-4">
+        <div className="w-full mt-8 gap-2 relative md:w-1/2 lg:w-1/3 flex flex-col justify-evenly items-center">
+          <img
+            className="w-[90%] h-48 object-cover rounded cursor-pointer"
+            src="https://images.unsplash.com/photo-1461988320302-91bde64fc8e4?ixid=2yJhcHBfaWQiOjEyMDd9&w=750&dpr=2"
+            alt=""
+          />
+          <div className="absolute top-0 bg-[#905f9262] h-48 w-[90%] hover:hidden rounded cursor-pointer"></div>
+          <div className="w-[90%] flex justify-between items-center">
+            <p className="text-2xl font-bold text-[#ccd6f6] cursor-pointer">
+              Project Name
+            </p>
+            <p className="text-2xl text-[#ccd6f6] flex gap-2">
+              <AiOutlineGithub className="hover:text-white cursor-pointer" />
+              <AiOutlineEye className="hover:text-white cursor-pointer" />
+            </p>
+          </div>
+          <p className="w-[90%] text-sm">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem,
+            quibusdam vero animi veritatis temporibus ut tempora velit inventore
+            aut cum deserunt excepturi. Optio iste ex placeat repellat eos
+            assumenda deleniti excepturi nam perspiciatis inventore quaerat ea
+            omnis autem corrupti dolores eveniet numquam recusandae tempore,
+            explicabo dignissimos soluta nostrum pariatur! Perspiciatis, eos
+            esse? Architecto rem eligendi labore quas maxime possimus incidunt?
+          </p>
+        </div>
+        <div className="w-full mt-8 gap-2 relative md:w-1/2 lg:w-1/3 flex flex-col justify-evenly items-center">
+          <img
+            className="w-[90%] h-48 object-cover rounded cursor-pointer"
+            src="https://images.unsplash.com/photo-1461988320302-91bde64fc8e4?ixid=2yJhcHBfaWQiOjEyMDd9&w=750&dpr=2"
+            alt=""
+          />
+          <div className="absolute top-0 bg-[#905f9262] h-48 w-[90%] hover:hidden rounded cursor-pointer"></div>
+          <div className="w-[90%] flex justify-between items-center">
+            <p className="text-2xl font-bold text-[#ccd6f6] cursor-pointer">
+              Project Name
+            </p>
+            <p className="text-2xl text-[#ccd6f6] flex gap-2">
+              <AiOutlineGithub className="hover:text-white cursor-pointer" />
+              <AiOutlineEye className="hover:text-white cursor-pointer" />
+            </p>
+          </div>
+          <p className="w-[90%] text-sm">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem,
+            quibusdam vero animi veritatis temporibus ut tempora velit inventore
+            aut cum deserunt excepturi. Optio iste ex placeat repellat eos
+            assumenda deleniti excepturi nam perspiciatis inventore quaerat ea
+            omnis autem corrupti dolores eveniet numquam recusandae tempore,
+            explicabo dignissimos soluta nostrum pariatur! Perspiciatis, eos
+            esse? Architecto rem eligendi labore quas maxime possimus incidunt?
+          </p>
+        </div>
+        <div className="w-full mt-8 gap-2 relative md:w-1/2 lg:w-1/3 flex flex-col justify-evenly items-center">
+          <img
+            className="w-[90%] h-48 object-cover rounded cursor-pointer"
+            src="https://images.unsplash.com/photo-1461988320302-91bde64fc8e4?ixid=2yJhcHBfaWQiOjEyMDd9&w=750&dpr=2"
+            alt=""
+          />
+          <div className="absolute top-0 bg-[#905f9262] h-48 w-[90%] hover:hidden rounded cursor-pointer"></div>
+          <div className="w-[90%] flex justify-between items-center">
+            <p className="text-2xl font-bold text-[#ccd6f6] cursor-pointer">
+              Project Name
+            </p>
+            <p className="text-2xl text-[#ccd6f6] flex gap-2">
+              <AiOutlineGithub className="hover:text-white cursor-pointer" />
+              <AiOutlineEye className="hover:text-white cursor-pointer" />
+            </p>
+          </div>
+          <p className="w-[90%] text-sm">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem,
+            quibusdam vero animi veritatis temporibus ut tempora velit inventore
+            aut cum deserunt excepturi. Optio iste ex placeat repellat eos
+            assumenda deleniti excepturi nam perspiciatis inventore quaerat ea
+            omnis autem corrupti dolores eveniet numquam recusandae tempore,
+            explicabo dignissimos soluta nostrum pariatur! Perspiciatis, eos
+            esse? Architecto rem eligendi labore quas maxime possimus incidunt?
+          </p>
+        </div>
+        <div className="w-full mt-8 gap-2 relative md:w-1/2 lg:w-1/3 flex flex-col justify-evenly items-center">
+          <img
+            className="w-[90%] h-48 object-cover rounded cursor-pointer"
+            src="https://images.unsplash.com/photo-1461988320302-91bde64fc8e4?ixid=2yJhcHBfaWQiOjEyMDd9&w=750&dpr=2"
+            alt=""
+          />
+          <div className="absolute top-0 bg-[#905f9262] h-48 w-[90%] hover:hidden rounded cursor-pointer"></div>
+          <div className="w-[90%] flex justify-between items-center">
+            <p className="text-2xl font-bold text-[#ccd6f6] cursor-pointer">
+              Project Name
+            </p>
+            <p className="text-2xl text-[#ccd6f6] flex gap-2">
+              <AiOutlineGithub className="hover:text-white cursor-pointer" />
+              <AiOutlineEye className="hover:text-white cursor-pointer" />
+            </p>
+          </div>
+          <p className="w-[90%] text-sm">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem,
+            quibusdam vero animi veritatis temporibus ut tempora velit inventore
+            aut cum deserunt excepturi. Optio iste ex placeat repellat eos
+            assumenda deleniti excepturi nam perspiciatis inventore quaerat ea
+            omnis autem corrupti dolores eveniet numquam recusandae tempore,
+            explicabo dignissimos soluta nostrum pariatur! Perspiciatis, eos
+            esse? Architecto rem eligendi labore quas maxime possimus incidunt?
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Projects
+export default Projects;
